2f0e9392a373e79bfe29e9dea93122db5edc7c45
dr-azizbek <115967245+dr-azizbek@users.noreply.github.com>
2023-02-09 22:57:03 +0500

    s

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+const Home = () => {
+    return (
+        <div className="home">
+            <Header/>
+        </div>
+    )
+}
+
+export default Home;
